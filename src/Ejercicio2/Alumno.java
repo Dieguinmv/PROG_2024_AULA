6e92c56e9e4912d28e2ca68100ea9f5e82d492bf
Mañana <Maana@Aula8-03>
2024-01-17 12:55:15 +0100

    CAMBIO DE CONSTRUCTOR A USO DE SETS
    -He cambiado tanto el start como la clase alumno,porque anteriormente
    tenia un constructor en la clase alumno para setear el nombre y
    edad,pero, el maestro nos informa que es mejor utilizar los setters

--- src/Ejercicio2/Alumno.java
+++ src/Ejercicio2/Alumno.java
@@ -6,12 +6,29 @@ public class Alumno {
 	private int edad;
 	
 	
-	
+	public String getNombre() {
+		return nombre;
+	}
 
-	public Alumno(String nombre,int edad) {
+
+
+
+	public void setNombre(String nombre) {
 		this.nombre = nombre;
+	}
+
+
+
+
+	public int getEdad() {
+		return edad;
+	}
+
+
+
+
+	public void setEdad(int edad) {
 		this.edad = edad;
-		
 	}
 
 
